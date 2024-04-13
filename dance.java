8c62f4219d22ac52162318d28ba2431b998507a8
HarryZhao <hzhao0117@gmail.com>
2024-04-12 23:34:22 -0700

    here come two bunny

--- dance.java
+++ dance.java
@@ -1,5 +1,8 @@
 class Dance{
     public static void main(String [] args){
         System.out.println("look there's a dancing bunny");
+        System.out.println("and another bunny get the money")
+
+        
     }
 }
